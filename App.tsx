abc814881dfe534d5462fbcd7d6c59afa947034b
David Drysdale-Wilson <david.drysdale-wilson@pod-point.com>
2022-07-28 08:06:26 +0100

    Some refinements

--- App.tsx
+++ App.tsx
@@ -115,7 +115,7 @@ const App = () => {
 
   const progressIndicator = () => {
     return (
-      <ActivityIndicator size="large" color='#2196F3' />
+      <ActivityIndicator size="large" color='#2196F3'/>
   )}
 
   const loginPage = () => {
@@ -124,19 +124,19 @@ const App = () => {
       style={styles.button}
       onPress={() => login()}
     >
-      <Text style={{ color: '#fff', textAlign: 'center' }}>Login</Text>
+      <Text style={styles.buttonText}>Login</Text>
     </TouchableOpacity>
   )}
 
   const profilePage = () => {
     return (
       <View style={styles.centeredSection}>
-        <Text style={{ textAlign: 'center', paddingBottom: 48}}>Name: {state.name}</Text>
+        <Text style={{textAlign: 'center', paddingBottom: 48}}>Name: {state.name}</Text>
         <TouchableOpacity
           style={styles.button}
           onPress={() => logout()}
         >
-          <Text style={{ color: '#fff', textAlign: 'center' }}>Logout</Text>
+          <Text style={styles.buttonText}>Logout</Text>
         </TouchableOpacity>
         </View>
   )}
@@ -145,7 +145,7 @@ const App = () => {
     <SafeAreaView style={{backgroundColor: '#2196F3'}}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.appbar}>
-        <Text style={{color:'white', fontSize: 20}}>
+        <Text style={{color:'white', fontSize: 20, fontWeight: '500'}}>
           OAuth ReactNative Demo
         </Text>
       </View>
@@ -182,100 +182,18 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#2196F3',
-    width: 140,
-    height: 30,
+    width: 70,
+    height: 38,
     color: 'white',
     justifyContent: 'center',
-  }
-});
-
-
-/*const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    borderRadius: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});*/
+});
 
 export default App;
 
